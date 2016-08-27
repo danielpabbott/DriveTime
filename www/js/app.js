a51517d838e7c95d0954706e7775dbe0b64b7e3d
Daniel Abbott <danielpabbott@gmail.com>
2016-08-27 13:32:22 -0600

    add login template

--- www/js/app.js
+++ www/js/app.js
@@ -77,9 +77,14 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         controller: 'AccountCtrl'
       }
     }
+  })
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html'
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/login');
 
 });
