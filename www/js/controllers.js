92a9446c03c00711002817920e3bdb22f2752b1e
Daniel Abbott <danielpabbott@gmail.com>
2016-08-30 16:20:21 -0600

    adding five tabs

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,6 +2,8 @@ angular.module('starter.controllers', [])
 
 .controller('FlightsCtrl', function($scope) {})
 
+.controller('DashboardCtrl', function($scope) {})
+
 .controller('ConcertsCtrl', function($scope) {})
 
 .controller('SportsCtrl', function($scope) {})
@@ -10,18 +12,14 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-})
+.controller('AccountCtrl', function($scope) {})
 
 .controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state) {
     $scope.data = {};
 
     $scope.login = function() {
       LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
-        $state.go('tab.flights');
+        $state.go('tab.dashboard');
       }).error(function(data) {
         var alertPopup = $ionicPopup.alert({
           title: 'Login failed!',
